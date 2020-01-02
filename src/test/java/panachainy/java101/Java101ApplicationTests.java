dcf9f1afdf2f5b3048fc2cde6e3255dca2ee1873
Panachainy <panachai.ny@gmail.com>
2020-01-02 22:48:03 +0700

    init

---
+++ src/test/java/panachainy/java101/Java101ApplicationTests.java
@@ -0,0 +1,13 @@
+package panachainy.java101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
